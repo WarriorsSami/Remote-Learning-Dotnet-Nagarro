6f0be63bd667ccc49c60723d275272edc0dcc3a6
WarriorsSami <sbarbutdica@gmail.com>
2021-11-22 20:14:28 +0200

    feat(use case): define look use case

---
+++ Homework2/VendingMachine/UseCases/LookUseCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using VendingMachine.Data;
+using VendingMachine.PresentationLayer;
+
+namespace VendingMachine.UseCases
+{
+    internal class LookUseCase: IUseCase
+    {
+        private readonly VendingMachineApplication _application;
+        private readonly ProductRepository _productRepository;
+        private readonly ShelfView _shelfView;
+
+        public string Name => "display";
+        public string Description => "Display the list of available products stored in the vending machine";
+        public bool CanExecute => !_application.UserIsLoggedIn;
+        
+        public LookUseCase(VendingMachineApplication application, ShelfView shelfView)
+        {
+            _application = application;
+            _shelfView = shelfView;
+            _productRepository = new ProductRepository();
+        }
+        
+        public void Execute()
+        {
+            _shelfView.DisplayProducts(_productRepository.GetAll());
+        }
+    }
+}
