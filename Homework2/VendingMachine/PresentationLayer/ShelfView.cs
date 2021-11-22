cef9ffeea6234303c22ed2ca39ba82e7b7b4bb0c
WarriorsSami <sbarbutdica@gmail.com>
2021-11-22 20:16:11 +0200

    feat(display): add display class for products

--- Homework2/VendingMachine/PresentationLayer/ShelfView.cs
+++ Homework2/VendingMachine/PresentationLayer/ShelfView.cs
@@ -1,7 +1,19 @@
-﻿namespace VendingMachine.PresentationLayer
+﻿using System;
+using System.Collections.Generic;
+using VendingMachine.Models;
+
+namespace VendingMachine.PresentationLayer
 {
-    public class ShelfView
+    internal class ShelfView: DisplayBase
     {
-        
+        public void DisplayProducts(IEnumerable<Product> products)
+        {
+            Console.WriteLine("Available products:");
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Name} - price: {item.Price}$, " +
+                                  $"colId: {item.ColumnId}, qty: {item.Quantity}");
+            }
+        }
     }
 }
