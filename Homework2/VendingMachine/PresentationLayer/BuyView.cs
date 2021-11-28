5bc78aec86cb7ee0ca0b4bdab7a0e1660af942cb
WarriorsSami <sbarbutdica@gmail.com>
2021-11-28 13:12:32 +0200

    feat(view): define buy view for product displaying

--- Homework2/VendingMachine/PresentationLayer/BuyView.cs
+++ Homework2/VendingMachine/PresentationLayer/BuyView.cs
@@ -1,7 +1,16 @@
-﻿namespace VendingMachine.PresentationLayer
+﻿using System;
+using VendingMachine.Models;
+
+namespace VendingMachine.PresentationLayer
 {
-    public class BuyView
+    internal class BuyView: DisplayBase
     {
-        
+        public void DisplayProduct(Product product)
+        {
+            DisplayLine("You have bought the following product:", ConsoleColor.Cyan);
+            DisplayLine($"{product.Name} - price: {product.Price}$, " +
+                        $"colId: {product.ColumnId}, qty: {product.Quantity}",
+                        ConsoleColor.Green);
+        }
     }
 }
