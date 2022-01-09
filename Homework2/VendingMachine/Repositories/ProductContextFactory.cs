af59f23dae0e691634f69b6d969d0456b3cd1ca4
WarriorsSami <sbarbutdica@gmail.com>
2022-01-09 14:26:22 +0200

    feat: define product persistent repository
    Create ProductRepository based on EF Core requirements, ProductContext and ProductContextFactory to initialize the ProductRepository with it inside the Bootstrapper based on the connection string provided by appsettings.json file

---
+++ Homework2/VendingMachine/Repositories/ProductContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace VendingMachine.Repositories
+{
+    internal class ProductContextFactory: IDesignTimeDbContextFactory<ProductContext>
+    {
+        public ProductContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build(); 
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+                        
+            var optionsBuilder = new DbContextOptionsBuilder<ProductContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new ProductContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Homework2/VendingMachine/Repositories/ProductPersistentRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VendingMachine.Interfaces.IRepositories;
+using VendingMachine.Models;
+
+namespace VendingMachine.Repositories
+{
+    internal class ProductPersistentRepository: IProductRepository
+    {
+        private readonly ProductContext _context;
+
+        public ProductPersistentRepository(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetById(int code)
+        {
+            return _context.Products.FirstOrDefault(p => p.ColumnId == code);
+        }
+
+        public void UpdateQuantity(int code, int quantity)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ColumnId == code);
+            if (product != null)
+            {
+                product.Quantity = quantity;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
