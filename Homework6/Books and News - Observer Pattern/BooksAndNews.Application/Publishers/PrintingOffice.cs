2d58f383d6119503037f7347dd6f97554d7b6290
WarriorsSami <sbarbutdica@gmail.com>
2022-02-02 11:23:05 +0200

    ops: add base project

---
+++ Homework6/Books and News - Observer Pattern/BooksAndNews.Application/Publishers/PrintingOffice.cs
@@ -0,0 +1,24 @@
+﻿using iQuest.BooksAndNews.Application.DataAccess;
+
+namespace iQuest.BooksAndNews.Application.Publishers
+{
+    // todo: This class must be implemented.
+
+    /// <summary>
+    /// This is the class that will publish books and newspapers.
+    /// It must offer a mechanism through which different other classes can subscribe ether
+    /// to books or to newspaper.
+    /// When a book or newspaper is published, the PrintingOffice must notify all the corresponding
+    /// subscribers.
+    /// </summary>
+    public class PrintingOffice
+    {
+        public PrintingOffice(IBookRepository bookRepository, INewspaperRepository newspaperRepository, ILog log)
+        {
+        }
+
+        public void PrintRandom(int bookCount, int newspaperCount)
+        {
+        }
+    }
+}
