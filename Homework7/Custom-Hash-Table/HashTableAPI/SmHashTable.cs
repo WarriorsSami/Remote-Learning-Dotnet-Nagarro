6ff0779d36fb4c6c46e49330108e75417da2adb9
WarriorsSami <sbarbutdica@gmail.com>
2022-03-01 12:54:37 +0200

    feat: create custom hashtable and unit tests

---
+++ Homework7/Custom-Hash-Table/HashTableAPI/SmHashTable.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Custom_Hash_Table.HashTableAPI
+{
+    internal class SmHashTable<TKey, TValue>: 
+        IHashTable<TKey, TValue>, IEnumerable<Tuple<int, TValue>>
+        where TKey: notnull
+    {
+        private readonly List<Tuple<int, TValue>> _table = new();
+
+        public SmHashTable(IEnumerable<Tuple<TKey, TValue>> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+            
+            foreach (var (key, value) in items)
+            {
+                Put(key, value);
+            }
+        }
+        
+        public SmHashTable()
+        {
+        }
+
+        public TValue Get(TKey key)
+        {
+            var keyHash = key.GetHashCode(); 
+            foreach (var (currentHash, value) in _table) 
+            { 
+                if (currentHash == keyHash) 
+                { 
+                    return value;
+                }
+            }
+            
+            throw new KeyNotFoundException();
+        }
+
+        public bool Put(TKey key, TValue value)
+        {
+                var keyHash = key.GetHashCode();
+                foreach (var (currentHash, _) in _table)
+                {
+                    if (currentHash == keyHash)
+                    {
+                        return false;
+                    }
+                }
+
+                _table.Add(new Tuple<int, TValue>(keyHash, value));
+                return true;
+        }
+
+        public bool Remove(TKey key)
+        {
+            try
+            {
+                var keyHash = key.GetHashCode();
+                foreach (var (currentHash, value) in _table)
+                {
+                    if (currentHash == keyHash)
+                    {
+                        return _table
+                            .Remove(new Tuple<int, TValue>(keyHash, value));
+                    }
+                }
+
+                return false;
+            }
+            catch (NullReferenceException e)
+            {
+                throw new NullReferenceException(e.Message);
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            try
+            {
+                var keyHash = key.GetHashCode();
+                foreach (var (currentHash, _) in _table)
+                {
+                    if (currentHash == keyHash)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            catch (NullReferenceException e)
+            {
+                throw new NullReferenceException(e.Message);
+            }
+        }
+
+        public int Count()
+        {
+            return _table.Count;
+        }
+
+        public IEnumerator<Tuple<int, TValue>> GetEnumerator()
+        {
+            foreach (var item in _table)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Homework7/UnitTests/UnitTest.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using Custom_Hash_Table.HashTableAPI;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class HashTableTests
+    {
+        private IHashTable<StubClass, string> _hashTable;
+        private static IEnumerable<Tuple<StubClass, string>> Data 
+        {
+            get
+            {
+                return new Tuple<StubClass, string>[]
+                {
+                    new(new StubClass("Stub 1", 1, true), "1"),
+                    new(new StubClass("Stub 2", 2, false), "2"),
+                    new(new StubClass("Stub 3", 3, true), "3"),
+                    new(new StubClass("Stub 4", 4, false), "4"),
+                };
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _hashTable = new SmHashTable<StubClass, string>(Data);
+        }
+        
+        [Test]
+        public void TestConstructor_WhenInputListIsNotNull()
+        {
+            foreach (var (key, _) in Data)
+            {
+                Assert.IsTrue(_hashTable.ContainsKey(key));
+            }
+        }
+        
+        [Test]
+        public void TestConstructor_WhenInputListIsNull()
+        {
+            IEnumerable<Tuple<StubClass, string>> inputList = null;
+
+            Assert.Throws<ArgumentNullException>(
+                () => _hashTable = new SmHashTable<StubClass, string>(inputList));
+        }
+        
+        [Test]
+        public void TestContainsKey_WhenKeyIsNotInHashTable()
+        {
+            var key = new StubClass("Stub 5", 5, true);
+            Assert.IsFalse(_hashTable.ContainsKey(key));
+        }
+        
+        [Test]
+        public void TestContainsKey_WhenKeyIsInHashTable()
+        {
+            var key = new StubClass("Stub 1", 1, true);
+            Assert.IsTrue(_hashTable.ContainsKey(key));
+        }
+        
+        [Test]
+        public void TestContainsKey_WhenKeyIsNull()
+        {
+            StubClass key = null;
+            Assert.Throws<NullReferenceException>(
+                () => _hashTable.ContainsKey(key));
+        }
+        
+        [Test]
+        public void TestGetValue_WhenKeyIsNotInHashTable()
+        {
+            var key = new StubClass("Stub 5", 5, true);
+            Assert.Throws<KeyNotFoundException>(
+                () => _hashTable.Get(key));
+        }
+        
+        [Test]
+        public void TestGetValue_WhenKeyIsInHashTable()
+        {
+            var key = new StubClass("Stub 1", 1, true);
+            Assert.AreEqual("1", _hashTable.Get(key));
+        }
+        
+        [Test]
+        public void TestGetValue_WhenKeyIsNull()
+        {
+            StubClass key = null;
+            Assert.Throws<NullReferenceException>(
+                () => _hashTable.Get(key));
+        }
+        
+        [Test]
+        public void TestPut_WhenKeyIsNotInHashTable()
+        {
+            var key = new StubClass("Stub 5", 5, true);
+            var value = "5";
+            Assert.IsTrue(_hashTable.Put(key, value));
+            Assert.AreEqual(value, _hashTable.Get(key));
+        }
+        
+        [Test]
+        public void TestPut_WhenKeyIsInHashTable()
+        {
+            var key = new StubClass("Stub 1", 1, true);
+            var value = "1";
+            Assert.IsFalse(_hashTable.Put(key, value));
+            Assert.AreEqual(value, _hashTable.Get(key));
+        }
+        
+        [Test]
+        public void TestPut_WhenKeyIsNull()
+        {
+            StubClass key = null;
+            var value = "5";
+            Assert.Throws<NullReferenceException>(
+                () => _hashTable.Put(key, value));
+        }
+        
+        [Test]
+        public void TestRemove_WhenKeyIsNotInHashTable()
+        {
+            var key = new StubClass("Stub 5", 5, true);
+            Assert.IsFalse(_hashTable.Remove(key));
+        }
+        
+        [Test]
+        public void TestRemove_WhenKeyIsInHashTable()
+        {
+            var key = new StubClass("Stub 1", 1, true);
+            Assert.IsTrue(_hashTable.Remove(key));
+            Assert.IsFalse(_hashTable.ContainsKey(key));
+        }
+        
+        [Test]
+        public void TestRemove_WhenKeyIsNull()
+        {
+            StubClass key = null;
+            Assert.Throws<NullReferenceException>(
+                () => _hashTable.Remove(key));
+        }
+        
+        [Test]
+        public void TestCount_WhenHashTableIsEmpty()
+        {
+            var hashTable = new SmHashTable<StubClass, string>();
+            Assert.AreEqual(0, hashTable.Count());
+        }
+        
+        [Test]
+        public void TestCount_WhenHashTableIsNotEmpty()
+        {
+            Assert.AreEqual(
+                new List<Tuple<StubClass, string>>(Data).Count, 
+                _hashTable.Count());
+        }
+    }
+}
