b6879d2af1e6669663a8585baa60e6bb2426a96c
WarriorsSami <sbarbutdica@gmail.com>
2022-04-25 22:58:45 +0300

    feat: implement idisposable using finalizers

---
+++ Homework14/Star Files - Disposable Pattern/StarFiles/UseCase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using iQuest.StarFiles.UniverseModel;
+
+namespace iQuest.StarFiles
+{
+    internal class UseCase
+    {
+        public void Execute()
+        {
+            Universe universe = null;
+            string starFilePath, binaryStarFilePath1, binaryStarFilePath2;
+
+            try
+            {
+                universe = new Universe();
+
+                starFilePath = GenerateStar(universe);
+                (binaryStarFilePath1, binaryStarFilePath2) = GenerateBinaryStar(universe);
+            }
+            finally
+            {
+                universe?.Dispose();
+            }
+
+            DisplayFileContent(starFilePath);
+            DisplayFileContent(binaryStarFilePath1);
+            DisplayFileContent(binaryStarFilePath2);
+        }
+
+        private static string GenerateStar(Universe universe)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Generating star.");
+
+            const string starName = "Betelgeuse";
+            var createdFilePath = universe.CreateStarFromTemplate(starName);
+
+            Console.WriteLine($"Finished generating star: '{starName}'.");
+            Console.WriteLine($"File: {createdFilePath}");
+            return createdFilePath;
+        }
+
+        private static Tuple<string, string> GenerateBinaryStar(Universe universe)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Generating binary star.");
+
+            const string starName = "Sirius";
+            var createdFilePath = universe.CreateBinaryStar(starName);
+
+            Console.WriteLine($"Finished generating binary star '{starName}'.");
+            Console.WriteLine($"Files: {createdFilePath}");
+            return createdFilePath;
+        }
+
+        private static void DisplayFileContent(string filePath)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"File: '{filePath}'");
+            Console.WriteLine("Content:");
+
+            using var winApiFile = new WinApiFile(filePath);
+            var text = winApiFile.ReadAll();
+            Console.WriteLine(text);
+        }
+    }
+}
